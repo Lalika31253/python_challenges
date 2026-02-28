28ee786e721ee6f9800b760237bfb7a375db67df
Alina <Lalika31253@gmail.com>
2026-02-27 23:49:39 -0700

    Add shopping receipt file

---
+++ challenge_04_shopping_receipt.py
@@ -0,0 +1,19 @@
+first_item = input("Enter item 1: ")
+price1 = float(input("Enter price: "))
+
+second_item = input("Enter item 2: ")
+price2 = float(input("Enter price: "))
+
+third_item = input("Enter item 3: ")
+price3 = float(input("Enter price: "))
+
+total = price1 + price2 + price3
+
+print(f"--------------------------")
+print (f"{first_item}    ${price1}")
+print (f"{second_item}    ${price2}")
+print (f"{third_item}    ${price3}")
+print(f"--------------------------")
+
+print(f"TOTAL           ${total}")
+
